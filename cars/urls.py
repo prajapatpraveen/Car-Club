d047736f69d96768bc138886e0e86e70ae603632
prajapatpraveen <pk004846@gmail.com>
2022-02-08 12:21:25 +0530

    search and fetch data

--- cars/urls.py
+++ cars/urls.py
@@ -1,5 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 urlpatterns=[
-    path(' ',views.Cars,name='cars'),
+    path('',views.Cars,name='cars'),
+    path('<int:id>',views.Car_details, name='car_details'),
+    path('search/',views.Search, name='search'),
 ]
